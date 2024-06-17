2c055b3cedc2f113fe6f88fe1a8510044e4c38e5
Christian-Rivera1 <christianadonayriveralopez@gmail.com>
2024-06-16 20:10:06 -0600

    Final-Funcional-Primer-Sprint

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,16 +9,23 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/logout/',views.exit,name='exit'),
     path('accounts/profile/',views.profile,name='profile'),
+    path('listar-estudiantes/',views.listar_estudiantes,name='listar_estudiantes'),
+    path('registrar-estudiantes/',views.registrar_estudiante,name='registrar_estudiante'),
+    path('editar-estudiantes/<int:id>/',views.editar_estudiante,name='editar_estudiante'),
+    path('eliminar_estudiante/<int:id>/', views.eliminar_estudiante, name='eliminar_estudiante'),
+
+
    
     path('listar-docentes/',views.listar_docentes,name='listardocentes'),
     #Codigo Menu administrador - Agregado por Daniel 
     
     
     path('registrodocente/', views.registrodocente, name='registrodocente'),
-    path('visualizardatosregistros/', views.visualizarregistro, name='visualizarregistro'),
+    #path('visualizardatosregistros/', views.visualizarregistro, name='visualizarregistro'),
     #Codigo Asignacion Docente - Daniel 
     path('administrarasignaciondocente/', views.administrarasignaciondocente, name='administrarasignaciondocente'),
-    
+    path('eliminarasignacion/<int:id>/', views.eliminarasignacion, name='eliminarasignacion'),
+    path('editarasignacion/<int:id>/', views.editarasignacion, name='editarasignacion'),
     path('visualizarasignaciondocente/', views.visualizarasignaciondocente, name='visualizarasignaciondocente'),
     
 ]
